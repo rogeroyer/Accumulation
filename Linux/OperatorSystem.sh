da3d0891390e1553bfe41741e245f039d3dc1ea9
roger <rogerluo387@gmail.com>
2017-11-11 23:58:05 +0800

    Update OperatorSystem.sh

--- Linux/OperatorSystem.sh
+++ Linux/OperatorSystem.sh
@@ -1,5 +1,7 @@
 16.设计一个shell程序，在每月第一天备份并压缩/etc目录的所有内容，存放在/root/bak目录里，且文件名
 为如下形式yymmdd_etc，yy为年，mm为月，dd为日。Shell程序fileback存放在/usr/bin目录下。
+
+Method_one:
 vim /usr/bin/fileback.sh
 #!/bin/bash
 #fileback.sh
@@ -14,5 +16,20 @@ mv $filename /root/bak/
 vim /etc/crontab 加入
 * * 1 * * root ./fileback.sh &
 
-
 http://blog.csdn.net/lile269/article/details/6658885
+
+
+
+Method_two:
+#!/bin/sh
+DIRNAME=`ls /root | grep bak`
+if [ -z "$DIRNAME" ] ; then
+mkdir /root/bak
+cd /root/bak
+fi
+YY=`date +%y`
+MM=`date +%m`
+DD=`date +%d`
+BACKETC=$YY$MM$DD_etc.tar.gz
+tar zcvf $BACKETC /etc
+echo "fileback finished!"
