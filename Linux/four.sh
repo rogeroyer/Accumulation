7dc3a70585207fe1e8fda225ab42e94926c6316c
roger <rogerluo387@gmail.com>
2017-11-20 23:02:58 +0800

    Create by roger.

---
+++ Linux/four.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+read -p "Please input filename:" filename
+# -b判断是否是块设备 -o逻辑或 -c/-u判断是否是字符文件 #
+if [ -b $filename -o -c $filename ]; # 注意if后面的空格 # 
+then
+        echo "$filename is a device file or charactor file!"
+        # cp -a 全复制 #        
+        cp -a $filename /dev/
+        echo "copy finished!"
+else
+        echo "$filename is not a device file!" 
+        exit 1
+fi
+
+
+# 命令：man mknod #
