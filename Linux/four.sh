41ca751ae358eba313da0027a0547718df06caf1
roger <rogerluo387@gmail.com>
2017-11-21 09:25:42 +0800

    Update four.sh

--- Linux/four.sh
+++ Linux/four.sh
@@ -3,12 +3,12 @@ read -p "Please input filename:" filename
 # -b判断是否是块设备 -o逻辑或 -c/-u判断是否是字符文件 #
 if [ -b $filename -o -c $filename ]; # 注意if后面的空格 # 
 then
-        echo "$filename is a device file or charactor file!"
+        echo "$filename is a device file or a charactor file!"
         # cp -a 全复制 #        
         cp -a $filename /dev/
         echo "copy finished!"
 else
-        echo "$filename is not a device file!" 
+        echo "$filename is not a device file or a charactor file!!" 
         exit 1
 fi
 
