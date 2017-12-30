529b3ec5bd9bcbdcd65364855a7f1c460e3ad665
roger <rogerluo387@gmail.com>
2017-12-30 17:25:20 +0800

    Update Question_one.sh

--- Linux/Question_one.sh
+++ Linux/Question_one.sh
@@ -1,72 +1,67 @@
 #!/bin/bash
-#author
-#this script is only for CentOS 6
-#check the OS
-
+# 先对系统进行判断，如果是Cent OS 64位，就继续运行
 platform=`uname -i`
 if [ $platform != "x86_64" ];then 
 	echo "this script is only for 64bit Operating System !"
 	exit 1
 fi
 echo "the platform is ok"
-version=`lsb_release -r |awk '{print substr($2,1,1)}'`
-if [ $version != 6 ];then
-	echo "this script is only for CentOS 6 !"
-	exit 1
-fi
-cat << EOF
-+---------------------------------------+
-|   your system is CentOS 6 x86_64      |
-|      start optimizing.......          |
-+---------------------------------------
-EOF
 
-#make the 163.com as the default yum repo
+# 先将系统的安装源设置为网易的
 mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
 wget http://mirrors.163.com/.help/CentOS6-Base-163.repo -O /etc/yum.repos.d/CentOS-Base.repo
 
-#add the third-party repo
-#add the epel
-rpm -Uvh http://download.Fedora.RedHat.com/pub/epel/6/x86_64/epel-release-6-5.noarch.rpm 
-rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6
+# 禁用GSSAPI来认证，也禁用DNS反向解析，加快SSH登陆速度
+sed -i 's/^GSSAPIAuthentication yes$/GSSAPIAuthentication no/' /etc/ssh/sshd_config
+sed -i 's/#UseDNS yes/UseDNS no/' /etc/ssh/sshd_config
+service sshd restar
 
-#add the rpmforge
-rpm -Uvh http://packages.sw.be/rpmforge-release/rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm
-rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-rpmforge-dag
-
-
-#set the file limit
+# 将系统同时打开的文件个数增大
 echo "ulimit -SHn 102400" >> /etc/rc.local
 cat >> /etc/security/limits.conf << EOF
 *           soft   nofile       65535
 *           hard   nofile       65535
 EOF
 
-#set the control-alt-delete to guard against the miSUSE
-sed -i 's#exec /sbin/shutdown -r now#\#exec /sbin/shutdown -r now#' /etc/init/control-alt-delete.conf
+# 优化一些内核参数
+#tune kernel parametres
+cat >> /etc/sysctl.conf << EOF
+net.ipv4.tcp_fin_timeout = 1
+net.ipv4.tcp_keepalive_time = 1200
+net.ipv4.tcp_mem = 94500000 915000000 927000000
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.tcp_timestamps = 0
+net.ipv4.tcp_synack_retries = 1
+net.ipv4.tcp_syn_retries = 1
+net.ipv4.tcp_tw_recycle = 1
+net.core.rmem_max = 16777216
+net.core.wmem_max = 16777216
+net.core.netdev_max_backlog = 262144
+net.ipv4.tcp_max_orphans = 3276800
+net.ipv4.tcp_max_syn_backlog = 262144
+net.core.wmem_default = 8388608
+net.core.rmem_default = 8388608
+EOF
+/sbin/sysctl -p
 
-#disable selinux
+# 关闭selinux
 sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
 
 
-#define the backspace button can erase the last character typed
+# 调整删除字符的按键为backspace（某些系统默认是delete）
 echo 'stty erase ^H' >> /etc/profile
 
+# 打开vim的语法高亮
 echo "syntax on" >> /root/.vimrc
 
-#stop some crontab
-mkdir /etc/cron.daily.bak
-mv /etc/cron.daily/makewhatis.cron /etc/cron.daily.bak
-mv /etc/cron.daily/mlocate.cron /etc/cron.daily.bak
+# 关闭没用的服务
 chkconfig bluetooth off
 chkconfig cups off
-chkconfig ip6tables off
 
-#disable the ipv6
+# 关闭IPv6
 cat > /etc/modprobe.d/ipv6.conf << EOFI
 alias net-pf-10 off
 options ipv6 disable=1
-
 EOFI
 echo "NETWORKING_IPV6=off" >> /etc/sysconfig/network
 cat << EOF
