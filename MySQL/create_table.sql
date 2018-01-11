14171e7e395ced2fb24ebcb045b66928816153d5
roger <rogerluo387@gmail.com>
2018-01-11 22:55:32 +0800

    Create create_table.sql

---
+++ MySQL/create_table.sql
@@ -0,0 +1,10 @@
+
+create table costomer( 
+  username varchar(20), 
+  password varchar(15), 
+  suggestion varchar(200), 
+  fruit varchar(10), 
+  sport varchar(40), 
+  city varchar(10), 
+  primary key(username)
+);
