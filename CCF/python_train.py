af1a5c593aeb7c0ccd82ecb4bfc05b7ddae03e4e
roger <rogerluo387@gmail.com>
2018-07-05 19:58:16 +0800

    Create python_train.py

---
+++ CCF/python_train.py
@@ -0,0 +1,13 @@
+
+length = int(sys.stdin.readline())
+array_input = sys.stdin.readline()
+array_input = [int(n) for n in array_input.split()]
+array_input = sorted(array_input)
+
+result = array_input[1] - array_input[0]
+for index in range(1, len(array_input) - 1, 1):
+    temp = array_input[index + 1] - array_input[index]
+    if temp < result:
+        result = temp
+
+sys.stdout.write(result)
