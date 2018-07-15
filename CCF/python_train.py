ea1232b80650d90e4e34ec8c59ec42b0e6b6d103
roger <rogerluo387@gmail.com>
2018-07-15 21:43:15 +0800

    Update python_train.py

--- CCF/python_train.py
+++ CCF/python_train.py
@@ -1,4 +1,6 @@
-
+'''
+Version one
+'''
 length = int(sys.stdin.readline())
 array_input = sys.stdin.readline()
 array_input = [int(n) for n in array_input.split()]
@@ -11,3 +13,20 @@ for index in range(1, len(array_input) - 1, 1):
         result = temp
 
 sys.stdout.write(result)
+
+
+'''
+Version two
+'''
+length = int(input())
+array_input = list(map(int, input().split(' ')))
+
+array_input = sorted(array_input)
+
+result = array_input[1] - array_input[0]
+for index in range(1, len(array_input) - 1, 1):
+    temp = array_input[index + 1] - array_input[index]
+    if temp < result:
+        result = temp
+
+print(result)
