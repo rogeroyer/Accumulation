3d0bf1a6e402bf935c09acb545795e2bf67f44b3
roger <rogerluo387@gmail.com>
2018-12-29 14:21:27 +0800

    Update plot_example.py

--- python/matplotlib/plot_example.py
+++ python/matplotlib/plot_example.py
@@ -4,17 +4,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.linspace(0, 10, 1000)    # 作图的变量自变量
-print(x)
-print(len(x))
+
 y = np.sin(x) + 1      # 因变量 y
 z = np.cos(x**2) + 1   # 因变量 z
 
-plt.figure(figsize = (8, 4))    # 设置图像大小
-plt.plot(x, y, label = '$\ sinx + 1$', color='red', linewidth=2)     # 作图设置标签、线条颜色、线条大小
+plt.rcParams['font.sans-serif'] = ['SimHei']    # 用于显示中文标签
+plt.rcParams['axes.unicode_minus'] = False      # 用于正常显示负号
+
+plt.figure(figsize=(8, 4))    # 设置图像大小
+plt.plot(x, y, label='$\ sinx + 1$', color='red', linewidth=2)     # 作图设置标签、线条颜色、线条大小
 plt.plot(x, z, 'b--', label='$\ cos x^2+1$')                          # 作图设置标签、线条类型
 plt.xlabel('Time(s)')    # x轴名称
 plt.ylabel('Volt')       # y轴名称
-plt.title('A simple example')     # 标题
+plt.title('A simple example 你好')     # 标题
 plt.ylim(-0.2, 2.2)         # 显示的y轴范围
 plt.legend()             # 显示图例
 plt.show()               # 显示作图结果
+
