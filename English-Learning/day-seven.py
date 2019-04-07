5e153e4653a33f85ae27d91908d3291e218fc628
roger <rogerluo387@gmail.com>
2019-04-07 08:54:10 +0800

    Create day-seven.py

---
+++ English-Learning/day-seven.py
@@ -0,0 +1,13 @@
+### DAY 7 语料
+
+1. a tough spot -> 处境艰难、困境
+2. with gusto -> 满腔热情
+3. rise above -> 克服
+4. conform to -> 遵守、符合
+5. work overtime -> 加班
+6. make for -> 有助于、导致
+7. be conducive to -> 有利于、有助于
+8. put up with -> 忍受
+9. be sb's highest priority -> 成为某人的首要
+10. without doing..., sb has no hope of doing -> 没有...就不可能做到
+
