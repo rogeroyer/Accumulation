d27c757990ffd6543caf96ea74c67d983b1d3f16
roger <rogerluo387@gmail.com>
2019-04-09 22:09:31 +0800

    Create enumerate.py

---
+++ python/util/enumerate.py
@@ -0,0 +1,7 @@
+
+
+seasons = ['Spring', 'Summer', 'Fall', 'Winter']
+list(enumerate(seasons))
+# [(0, 'Spring'), (1, 'Summer'), (2, 'Fall'), (3, 'Winter')]
+list(enumerate(seasons, start=1))       # 下标从 1 开始
+# [(1, 'Spring'), (2, 'Summer'), (3, 'Fall'), (4, 'Winter')]
