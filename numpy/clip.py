ccaa860a1b727e7235c01739fe8849d7d147b34e
roger <rogerluo387@gmail.com>
2019-11-12 20:29:13 +0800

    Update clip.py

--- numpy/clip.py
+++ numpy/clip.py
@@ -1,5 +1,11 @@
-#Example-1
+"""
+Clip：如何使数组中的值保持在一定区间内
+"""
+
+# Example-1
 array = np.array([10, 7, 4, 3, 2, 2, 5, 9, 0, 4, 6, 0])
-print (np.clip(array,2,6))[6 6 4 3 2 2 5 6 2 4 6 2]#Example-2
+print (np.clip(array,2,6))[6 6 4 3 2 2 5 6 2 4 6 2]
+
+# Example-2
 array = np.array([10, -1, 4, -3, 2, 2, 5, 9, 0, 4, 6, 0])
 print (np.clip(array,2,5))[5 2 4 2 2 2 5 5 2 4 5 2]
